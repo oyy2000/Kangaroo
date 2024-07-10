3d28a3853e8e18510041df05e029a210e6ec71cd
yangOuyang <858452228@qq.com>
2024-07-10 11:11:09 -0400

    optimize the generation process

--- evaluation/inference_kangaroo_privacy.py
+++ evaluation/inference_kangaroo_privacy.py
@@ -11,13 +11,16 @@ import time
 import numpy as np
 import shortuuid
 
-from fastchat.llm_judge.common import load_questions
-from fastchat.model import get_conversation_template
-from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from kangaroo.kangaroo_model import KangarooModel
+from fastchat.model import get_conversation_template
+from tqdm import tqdm
 
-def run_eval(
+def save_json(data, file_path):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+def generation(
         model,
         tokenizer,
         forward_func,
@@ -32,20 +35,21 @@ def run_eval(
         num_gpus_total,
         **kwargs,
 ):
-    questions = load_questions(question_file, question_begin, question_end)
+    with open(question_file) as f:
+        original_data = json.load(f)
+
+    if os.path.exists(answer_file):
+        with open(answer_file, 'r') as f:
+            res_data = json.load(f)
+    else:
+        res_data = original_data
+
+    questions = res_data
 
     # Split the question file into `num_gpus` files
     assert num_gpus_total % num_gpus_per_model == 0
-    use_ray = num_gpus_total // num_gpus_per_model > 1
 
-    if use_ray:
-        import ray
-        ray.init()
-        get_answers_func = ray.remote(num_gpus=num_gpus_per_model)(
-            get_model_answers
-        ).remote
-    else:
-        get_answers_func = get_model_answers
+    get_answers_func = get_model_answers
 
     chunk_size = len(questions) // (num_gpus_total // num_gpus_per_model)  # // 2
     ans_handles = []
@@ -64,8 +68,6 @@ def run_eval(
             )
         )
 
-    if use_ray:
-        ray.get(ans_handles)
 
 
 @torch.inference_mode()
@@ -90,108 +92,90 @@ def get_model_answers(
     accept_lengths_tree = []
     for question in tqdm(questions):
         choices = []
-        for i in range(num_choices):
-            cur_accept_lengths_tree = []
-            # torch.manual_seed(i)
-            conv = get_conversation_template("vicuna")
-            turns = []
-            idxs = []
-            new_tokens = []
-            wall_time = []
-            q_prompt = question["prompt"]
-            conv.append_message(conv.roles[0], q_prompt)
-            conv.append_message(conv.roles[1], None)
-            prompt = conv.get_prompt()
-            inputs = tokenizer([prompt], return_tensors="pt").to("cuda")
-            input_ids = inputs.input_ids
-            try:
-                torch.cuda.synchronize()
-                start_time = time.time()
-                output_ids, new_token, idx, accept_length_tree = forward_func(
-                    inputs,
-                    model,
-                    tokenizer,
-                    max_new_tokens,
-                    **kwargs,
-                )
-                torch.cuda.synchronize()
-                total_time = time.time() - start_time
-                accept_lengths_tree.extend(accept_length_tree)
-                output_ids = output_ids[0][len(input_ids[0]):]
-            except RuntimeError as e:
-                print("ERROR when forwarding question ID: ", question["question_id"])
-                output = "ERROR"
+        cur_accept_lengths_tree = []
+        # torch.manual_seed(i)
+        conv = get_conversation_template("vicuna")
+        turns = []
+        idxs = []
+        new_tokens = []
+        wall_time = []
+        q_prompt = question["prompt"]
+        conv.append_message(conv.roles[0], q_prompt)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+        inputs = tokenizer([prompt], return_tensors="pt").to("cuda")
+        input_ids = inputs.input_ids
+        try:
+            torch.cuda.synchronize()
+            start_time = time.time()
+            output_ids, new_token, idx, accept_length_tree = forward_func(
+                inputs,
+                model,
+                tokenizer,
+                max_new_tokens,
+                **kwargs,
+            )
+            torch.cuda.synchronize()
+            total_time = time.time() - start_time
+            accept_lengths_tree.extend(accept_length_tree)
+            output_ids = output_ids[0][len(input_ids[0]):]
+        except RuntimeError as e:
+            print("ERROR when forwarding question: ", question["prompt"])
+            output = "ERROR"
 
-            try:
-                if conv.stop_token_ids:
-                    stop_token_ids_index = [
-                        i
-                        for i, id in enumerate(output_ids)
-                        if id in conv.stop_token_ids
-                    ]
-                    if len(stop_token_ids_index) > 0:
-                        output_ids = output_ids[: stop_token_ids_index[0]]
+        try:
+            if conv.stop_token_ids:
+                stop_token_ids_index = [
+                    i
+                    for i, id in enumerate(output_ids)
+                    if id in conv.stop_token_ids
+                ]
+                if len(stop_token_ids_index) > 0:
+                    output_ids = output_ids[: stop_token_ids_index[0]]
 
-                output = tokenizer.decode(
-                    output_ids,
-                    spaces_between_special_tokens=False,
-                )
-                if conv.stop_str and output.find(conv.stop_str) > 0:
-                    output = output[: output.find(conv.stop_str)]
-                for special_token in tokenizer.special_tokens_map.values():
-                    if isinstance(special_token, list):
-                        for special_tok in special_token:
-                            output = output.replace(special_tok, "")
-                    else:
-                        output = output.replace(special_token, "")
-                output = output.strip()
+            output = tokenizer.decode(
+                output_ids,
+                spaces_between_special_tokens=False,
+            )
+            if conv.stop_str and output.find(conv.stop_str) > 0:
+                output = output[: output.find(conv.stop_str)]
+            for special_token in tokenizer.special_tokens_map.values():
+                if isinstance(special_token, list):
+                    for special_tok in special_token:
+                        output = output.replace(special_tok, "")
+                else:
+                    output = output.replace(special_token, "")
+            output = output.strip()
 
-                if conv.name == "xgen" and output.startswith("Assistant:"):
-                    output = output.replace("Assistant:", "", 1).strip()
-            except RuntimeError as e:
-                print("ERROR question ID: ", question["question_id"])
-                output = "ERROR"
+            if conv.name == "xgen" and output.startswith("Assistant:"):
+                output = output.replace("Assistant:", "", 1).strip()
+        except RuntimeError as e:
+            print("ERROR question: ", question["prompt"])
+            output = "ERROR"
 
-            turns.append(output)
-            idxs.append(int(idx))
-            new_tokens.append(int(new_token))
-            wall_time.append(total_time)
-            cur_accept_lengths_tree.extend(accept_length_tree)
-            conv.messages[-1][-1] = output
-            # torch.cuda.empty_cache()
-            choices.append({"index": i, "turns": turns, "idxs": idxs, "new_tokens": new_tokens, "wall_time": wall_time,
-                            "accept_lengths": cur_accept_lengths_tree})
+        turns.append(output)
+        idxs.append(int(idx))
+        new_tokens.append(int(new_token))
+        wall_time.append(total_time)
+        cur_accept_lengths_tree.extend(accept_length_tree)
+        conv.messages[-1][-1] = output
+        # torch.cuda.empty_cache()
+        # choices.append({"index": i, "turns": turns, "idxs": idxs, "new_tokens": new_tokens, "wall_time": wall_time,
+        #                 "accept_lengths": cur_accept_lengths_tree})
 
         # Dump answers
-        os.makedirs(os.path.dirname(answer_file), exist_ok=True)
-        with open(os.path.expanduser(answer_file), "a") as fout:
-            ans_json = {
-                "question_id": question["question_id"],
-                "label": question["label"],
-                "answer_id": shortuuid.uuid(),
-                "model_id": model_id,
-                "choices": choices,
-                "tstamp": time.time(),
-                "res": output,
-                "prompt": q_prompt,
-                "source": question["source"]
-            }
-            fout.write(json.dumps(ans_json) + "\n")
+        try:
+            # If 'res' key doesn't exist or its value is empty, generate a new response
+            if "res" not in question or not question['res']:
+                question['res'] = output
+        except Exception as e:
+            # Print error message if there's an issue during processing
+            print(f"Error processing prompt {question['prompt']}: {e}")
+
+        save_json(questions, answer_file)
+    
     print("#Mean accepted tokens: ", np.mean(accept_lengths_tree))
 
-
-def reorg_answer_file(answer_file):
-    """Sort by question id and de-duplication"""
-    answers = {}
-    with open(answer_file, "r") as fin:
-        for l in fin:
-            qid = json.loads(l)["question_id"]
-            answers[qid] = l
-
-    qids = sorted(list(answers.keys()))
-    with open(answer_file, "w") as fout:
-        for qid in qids:
-            fout.write(answers[qid])
 
 
 def kangaroo_forward(inputs, model, tokenizer, max_new_tokens, do_sample=False, max_length = 2048, EARLY_STOP_LAYER = 2, SPECULATIVE_DECODING_STEPS = 6, threshold = 0.6):
@@ -391,7 +375,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    question_file = f"jailbreak.jsonl"
+    question_file = f"data/privacy_leakage.json"
 
     model = KangarooModel(args.model_path, args.adapter_path, args, EARLY_STOP_LAYER = args.exitlayer)
     tokenizer = AutoTokenizer.from_pretrained(args.model_path)
@@ -400,27 +384,27 @@ if __name__ == "__main__":
     assert not args.answer_file
     os.makedirs(f"data/{args.bench_name}/{args.model_id}", exist_ok=True)
 
-    for run in range(1):
-        answer_file = f"data/{args.bench_name}/{args.model_id}/{run}_safety.jsonl"
-        print(f"Output to {answer_file}")
+    answer_file = f"data/{args.bench_name}/{args.model_id}/privacy_result.jsonl"
+    
+    print(f"Output to {answer_file}")
 
-        run_eval(
-            model=model,
-            tokenizer=tokenizer,
-            forward_func=kangaroo_forward,
-            model_id=args.model_id,
-            question_file=question_file,
-            question_begin=args.question_begin,
-            question_end=args.question_end,
-            answer_file=answer_file,
-            max_new_tokens=args.max_new_tokens,
-            num_choices=args.num_choices,
-            num_gpus_per_model=args.num_gpus_per_model,
-            num_gpus_total=args.num_gpus_total,
-            do_sample=do_sample,
-            threshold=args.threshold,
-            SPECULATIVE_DECODING_STEPS=args.steps,
-            EARLY_STOP_LAYER=args.exitlayer
-        )
+    generation(
+        model=model,
+        tokenizer=tokenizer,
+        forward_func=kangaroo_forward,
+        model_id=args.model_id,
+        question_file=question_file,
+        question_begin=args.question_begin,
+        question_end=args.question_end,
+        answer_file=answer_file,
+        max_new_tokens=args.max_new_tokens,
+        num_choices=args.num_choices,
+        num_gpus_per_model=args.num_gpus_per_model,
+        num_gpus_total=args.num_gpus_total,
+        do_sample=do_sample,
+        threshold=args.threshold,
+        SPECULATIVE_DECODING_STEPS=args.steps,
+        EARLY_STOP_LAYER=args.exitlayer
+    )
 
-        reorg_answer_file(answer_file)
+    # reorg_answer_file(answer_file)
